10e3465f3a44add8634c56885ee65f65438fdb58
vitir._ <140893548+vitir0@users.noreply.github.com>
2025-07-02 11:39:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.environ['8004274832:AAG2gDVDp_dQLllcVBIYVB-0WTJ1Ts4CtCU']
 PORT = int(os.environ.get('PORT', 5000))
-WEBHOOK_URL = os.environ['WEBHOOK_URL'] + '/'  # Важно: добавить слэш в конце
+WEBHOOK_URL = os.environ['https://mypc-wk16.onrender.com'] + '/'  # Важно: добавить слэш в конце
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
